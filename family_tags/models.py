d2ba5ff2d593e9c26e37f1177589965266e903b6
Hilton Shumway <hillshum@gmail.com>
2015-05-11 21:25:07 -0600

    Add some basic models

--- family_tags/models.py
+++ family_tags/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.FileField()
+    caption = models.CharField(max_length=150)
+    taken_date = models.DateTimeField('date taken')
+
+    def __str__(self):
+        return self.caption
+
+
+class Ancestor(models.Model):
+    given_names = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=40)
+
+    photos = models.ManyToManyField(Photo, through="PictureRelationship")
+
+    def __str__(self):
+        return self.given_names + self.last_name
+
+
+class PictureRelationship(models.Model):
+    """Defining this here because I'll want to store info on it later"""
+    ancestor = models.ForeignKey(Ancestor)
+    photo = models.ForeignKey(Photo)
